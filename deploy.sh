f34e76d10acfb31ef184f1e3bbf9fe88d708622a
Ochaworldwide <lazarusgabriel000@gmail.com>
2024-05-09 08:39:32 +0100

    updated

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+# echo " Relax for now "
+
+# git pull
+
+# echo " Pulling from git "
+
+# npm run build
+
+# echo " Building the project "
+
+
+# scp -r dist/* ubuntu@44.202.158.101:/var/www/westempire.online/html
+
+# echo "Successful"
+
+git add .
+
+git commit -m "updated"
+
+git push -u origin main
